25babdd388686e9526fe810e04429a84f99de856
Patricia González García <patriciagonzalezinfo@gmail.com>
2026-02-13 13:19:18 +0100

    Cambiar camara al explotar el personaje y después restaurar la otra camara y al personaje de nuevo

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -6,18 +6,47 @@ public class Explosion : MonoBehaviour
 	#region Properties
 	#endregion
 
-	#region Fields
+	#region 
+	[SerializeField] private Camera _mainCamera;
+	[SerializeField] private Camera _explosionCamera;
 	[SerializeField] private GameObject _explosionEffect;
-	[SerializeField] private GameObject _explosionTarget;
+	[SerializeField] private GameObject _explosionPlayer;
 	[SerializeField] private float _explosionArea;
 	[SerializeField] private float _explosionForce;
 	#endregion
 
 	#region Unity Callbacks
+	private void Update()
+	{
+		if(_explosionEffect != null)
+		{
+			_explosionCamera.transform.LookAt(_explosionPlayer.transform.position);
+			_explosionCamera.transform.Translate(_explosionCamera.transform.forward * Time.deltaTime * 2f, Space.Self);
+			if (Vector3.Distance(_explosionCamera.transform.position, _explosionPlayer.transform.position) < 3)
+			{
+				_explosionCamera.enabled = false;
+				_mainCamera.enabled = true;
+				Vector3 currentPos = _explosionPlayer.transform.position;
+				_explosionPlayer.transform.localPosition = Vector3.zero;
+				_explosionPlayer.transform.GetComponent<CharacterController>().enabled = false;
+				_explosionPlayer.transform.position = currentPos;
+				_explosionPlayer.transform.GetComponent<CharacterController>().enabled = true;
+				_explosionPlayer.transform.GetComponent<Animator>().enabled = true;
+				Destroy(gameObject);
+				//Vector3 currentPos = _explosionPlayer.transform.position;
+				//_explosionPlayer.transform.position = currentPos;
+			}
+		}
+	}
 	private void OnTriggerEnter(Collider other)
 	{
-		if (other.gameObject == _explosionTarget)
+		if (other.gameObject == _explosionPlayer)
 		{
+			// Switch cameras
+			_mainCamera.enabled = false;
+			_explosionCamera.enabled = true;
+
+			// Activate explosion effect
 			_explosionEffect.SetActive(true);
 			Animator playerAnim = other.GetComponent<Animator>();
 			if (playerAnim != null)
